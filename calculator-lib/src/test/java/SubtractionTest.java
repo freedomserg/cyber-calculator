9f421214dc43daf3db55f8161085727b80dd842a
freedomserg <freedomserg97@gmail.com>
2016-04-26 21:00:51 +0300

    calculator-lib: add tests for Subtracting

--- calculator-lib/src/test/java/SubtractionTest.java
+++ calculator-lib/src/test/java/SubtractionTest.java
@@ -5,7 +5,7 @@ import static org.hamcrest.core.Is.is;
 public class SubtractionTest {
 
     @org.junit.Test
-    public void testSubtracting() {
+    public void testIntegerSubtracting() {
         IntegerArguments args = new IntegerArguments();
         args.setFirstArg(7);
         args.setSecondArg(6);
@@ -13,4 +13,30 @@ public class SubtractionTest {
         Assert.assertThat(actual, is(1));
     }
 
+    @org.junit.Test
+    public void testLongSubtracting() {
+        LongArguments args = new LongArguments();
+        args.setFirstArg(7_000_000_000L);
+        args.setSecondArg(6_000_000_000L);
+        long actual = new Subtraction(1).getResult(args);
+        Assert.assertThat(actual, is(1_000_000_000L));
+    }
+
+    @org.junit.Test
+    public void testDoubleSubtracting() {
+        DoubleArguments args = new DoubleArguments();
+        args.setFirstArg(20.5);
+        args.setSecondArg(13.5);
+        double actual = new Subtraction(1).getResult(args);
+        Assert.assertThat(actual, is(7.0));
+    }
+
+    @org.junit.Test
+    public void testFloatSubtracting() {
+        FloatArguments args = new FloatArguments();
+        args.setFirstArg(10.0f);
+        args.setSecondArg(9.5f);
+        float actual = new Subtraction(1).getResult(args);
+        Assert.assertThat(actual, is(0.5f));
+    }
 }
