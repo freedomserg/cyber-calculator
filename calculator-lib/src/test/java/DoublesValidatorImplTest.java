996c11694efd31f423a39820f8c6df6bc73f6222
freedomserg <freedomserg97@gmail.com>
2016-04-28 14:43:32 +0300

    calculator-lib: tested unaty operations fot IntegersValidatorImpl and DoublesValidatorImpl

--- calculator-lib/src/test/java/DoublesValidatorImplTest.java
+++ calculator-lib/src/test/java/DoublesValidatorImplTest.java
@@ -10,20 +10,36 @@ public class DoublesValidatorImplTest {
         doublesOperations = new ExecutedCalculatorOperations();
         doublesOperations.addOperation("+", new Addition(1));
         doublesOperations.addOperation("-", new Subtraction(1));
+        doublesOperations.addOperation("sqr", new Squaring(3));
+        doublesOperations.addOperation("sqrt", new SquareRoot(3));
+        doublesOperations.addOperation("ln", new NaturalLogarithm(3));
     }
 
-    @org.junit.Test
+    @Test
     public void testValidatorWithCorrectExpression() {
         input = "7.12 + 45.56 - ( 12.9 - 6.2 )";
         Validator validator = new DoublesValidatorImpl();
         validator.validateExpressionAndCheckAcceptableOperations(input, doublesOperations);
     }
 
-    @org.junit.Test(expected = InvalidInputExpressionCalculatorException.class)
+    @Test(expected = InvalidInputExpressionCalculatorException.class)
     public void testValidatorWithIncorrectExpression() {
         input = "18,78 - 13.33? + { 16.75 - 6.1 }";
         Validator validator = new DoublesValidatorImpl();
         validator.validateExpressionAndCheckAcceptableOperations(input, doublesOperations);
     }
 
+    @Test
+    public void testValidateDoublesOperations() {
+        input = "sqr 1.5 - ( sqrt 33.3 - ln 331.69 ) + 75.44";
+        Validator validator = new DoublesValidatorImpl();
+        validator.validateExpressionAndCheckAcceptableOperations(input, doublesOperations);
+    }
+
+    @Test(expected = InvalidInputExpressionCalculatorException.class)
+    public void testIncorrectDoubleOperation() {
+        input = "17.4 ! + sqr 3.3";
+        Validator validator = new DoublesValidatorImpl();
+        validator.validateExpressionAndCheckAcceptableOperations(input, doublesOperations);
+    }
 }
--- calculator-lib/src/test/java/IntegersValidatorImplTest.java
+++ calculator-lib/src/test/java/IntegersValidatorImplTest.java
@@ -1,29 +1,47 @@
 import calculatorExceptions.InvalidInputExpressionCalculatorException;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class IntegersValidatorImplTest {
     private String input;
-    static ExecutedCalculatorOperations integersOperations;
+    private static ExecutedCalculatorOperations integersOperations;
 
     @BeforeClass
-    public static void initDoublesExecutedOperations() {
+    public static void initIntegersExecutedOperations() {
         integersOperations = new ExecutedCalculatorOperations();
         integersOperations.addOperation("+", new Addition(1));
         integersOperations.addOperation("-", new Subtraction(1));
+        integersOperations.addOperation("sqr", new Squaring(3));
+        integersOperations.addOperation("sqrt", new SquareRoot(3));
+        integersOperations.addOperation("ln", new NaturalLogarithm(3));
+        integersOperations.addOperation("!", new Factorial(3));
     }
 
-    @org.junit.Test
+    @Test
     public void testValidatorWithCorrectExpression() {
         input = "33 - ( 197 - ( 22 + 67 ) )";
         Validator validator = new IntegersValidatorImpl();
         validator.validateExpressionAndCheckAcceptableOperations(input, integersOperations);
     }
 
-    @org.junit.Test(expected = InvalidInputExpressionCalculatorException.class)
+    @Test(expected = InvalidInputExpressionCalculatorException.class)
     public void testValidatorWithIncorrectExpression() {
         input = "336 - 75.5 + !12 - 3 )";
         Validator validator = new IntegersValidatorImpl();
         validator.validateExpressionAndCheckAcceptableOperations(input, integersOperations);
     }
 
+    @Test
+    public void testValidateIntegerOperations() {
+        input = "ln 15 + ( sqrt 64 - sqr 10 + 2 ! )";
+        Validator validator = new IntegersValidatorImpl();
+        validator.validateExpressionAndCheckAcceptableOperations(input, integersOperations);
+    }
+
+    @Test(expected = InvalidInputExpressionCalculatorException.class)
+    public void testIncorrectIntegerOperation() {
+        input = "sqrt 64 - sin 90";
+        Validator validator = new IntegersValidatorImpl();
+        validator.validateExpressionAndCheckAcceptableOperations(input, integersOperations);
+    }
 }
