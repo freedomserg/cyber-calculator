fc4702afdac710a26af0ff40d2e93dfe9bc58fef
freedomserg <freedomserg97@gmail.com>
2016-04-28 15:21:44 +0300

    calculator-app: tested single binary operations, unary operations for CyberCalculator

--- calculator-app/src/test/java/CyberCalculatorTest.java
+++ calculator-app/src/test/java/CyberCalculatorTest.java
@@ -19,7 +19,46 @@ public class CyberCalculatorTest {
     }
 
     @Test
-    public void testWithkAllOperations() {
+    public void testAddition() {
+        infixNotation = "3.0 + 5";
+        double actual = (double)calculator.evaluate(infixNotation);
+        Assert.assertThat(actual, is(8.0));
+    }
+
+    @Test
+    public void testSubtraction() {
+        infixNotation = "17.3 - 5.6";
+        double result = (double)calculator.evaluate(infixNotation);
+        double actual = new BigDecimal(result).setScale(2, RoundingMode.DOWN).doubleValue();
+        Assert.assertThat(actual, is(11.7));
+    }
+
+    @Test
+    public void testAdditionAndSubtractionWithBrackets() {
+        infixNotation = "5.0 - ( 3.1 - 1.6 ) + 2.4";
+        double result = (double)calculator.evaluate(infixNotation);
+        double actual = new BigDecimal(result).setScale(2, RoundingMode.DOWN).doubleValue();
+        Assert.assertThat(actual, is(5.9));
+    }
+
+    @Test
+    public void testMultiplication() {
+        infixNotation = "2.0 * 3.0";
+        double result = (double)calculator.evaluate(infixNotation);
+        double actual = new BigDecimal(result).setScale(2, RoundingMode.DOWN).doubleValue();
+        Assert.assertThat(actual, is(6.0));
+    }
+
+    @Test
+    public void testDivision() {
+        infixNotation = "8.5 / 1.5";
+        double result = (double)calculator.evaluate(infixNotation);
+        double actual = new BigDecimal(result).setScale(2, RoundingMode.UP).doubleValue();
+        Assert.assertThat(actual, is(5.67));
+    }
+
+    @Test
+    public void testAdditSubtrMultiplDivOperations() {
         infixNotation = "3 * 2.5 - 5 / 3.75 + 4 / 1.25 * 2.2";
         double result = (double)calculator.evaluate(infixNotation);
         double actual = new BigDecimal(result).setScale(2, RoundingMode.UP).doubleValue();
@@ -27,10 +66,42 @@ public class CyberCalculatorTest {
     }
 
     @Test
-    public void testkWithAllOperationsAndBrackets() {
+    public void testAdditSubtrMultiplDivOperationsAndBrackets() {
         infixNotation = "6.54 * ( ( 22.25 - 8.95 ) / 4.2 * ( 99.87 - 31.94 ) ) * ( 1.9 - 3.12 )";
         double result = (double)calculator.evaluate(infixNotation);
         double actual = new BigDecimal(result).setScale(2, RoundingMode.DOWN).doubleValue();
         Assert.assertThat(actual, is(-1716.33));
+    }
+
+    @Test
+    public void testSquaring() {
+        infixNotation = "sqr 2.0";
+        double result = (double)calculator.evaluate(infixNotation);
+        double actual = new BigDecimal(result).setScale(2, RoundingMode.DOWN).doubleValue();
+        Assert.assertThat(actual, is(4.0));
+    }
+
+    @Test
+    public void testSquareRoot() {
+        infixNotation = "sqrt 2.25";
+        double result = (double)calculator.evaluate(infixNotation);
+        double actual = new BigDecimal(result).setScale(2, RoundingMode.DOWN).doubleValue();
+        Assert.assertThat(actual, is(1.5));
+    }
+
+    @Test
+    public void testNaturalLogarithm() {
+        infixNotation = "ln 50.78";
+        double result = (double)calculator.evaluate(infixNotation);
+        double actual = new BigDecimal(result).setScale(2, RoundingMode.UP).doubleValue();
+        Assert.assertThat(actual, is(3.93));
+    }
+
+    @Test
+    public void testComplexExpression() {
+        infixNotation = "ln 12.25 / ( sqrt 125.66 - sqr 2.9 * 3.74 )";
+        double result = (double)calculator.evaluate(infixNotation);
+        double actual = new BigDecimal(result).setScale(3, RoundingMode.UP).doubleValue();
+        Assert.assertThat(actual, is(-0.124));
     }
 }
